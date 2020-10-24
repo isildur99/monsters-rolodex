dd80404d5c1a64dfed276993a384b3287eb5e493
Isildur <isildurgreen@plancksec.com>
2020-10-24 19:38:34 -0400

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import { CardList } from './components/card-list/card-list.component'
+import SearchBox from "./components/search-box/search-box.component";
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      monsters: [],
+      searchField: '',
+    }
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  handleOnClick = () => {
+    this.setState({
+      string: 'Fifi',
+
+    })
+  }
+
+  handleOnChange = (e) => {
+    this.setState({searchField: e.target.value}, () =>{
+      console.log(this.state.searchField)
+    })
+
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}))
+  }
+
+  render(){
+    const {monsters, searchField } = this.state
+    const filteredMonsters = monsters.filter( monster => (
+        monster.name.toLowerCase().includes(searchField.toLowerCase())
+    ))
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>ROLERDEX</h1>
+          <SearchBox
+            placeholder='search monsters'
+            handlechange={this.handleOnChange}/>
+          <CardList monsters={filteredMonsters}/>
+          <p>
+            {this.state.string}
+          </p>
+          <button onClick={this.handleOnClick}> Click Me!</button>
+    
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
